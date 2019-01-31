70c545fe3e14ae8957019eece54f51c9b8d1fa4f
Uzair Ali <uzaira960@gmail.com>
2019-01-31 18:29:45 +0000

    implementing stable marriage algorithm

--- stable_marriage.py
+++ stable_marriage.py
@@ -1,10 +1,11 @@
 class Mentor(object):
 
-	def __init__(self, gender, course_code, student_id, interests):
+	def __init__(self, gender, course_code, student_id, interests, preferences=[]):
 		self.gender = gender
 		self.course_code = course_code
 		self.student_id = student_id
 		self.interests = interests
+		self.preferences = []
 
 	def __str__(self):
 		return f"Mentor: {self.student_id}"
@@ -21,15 +22,47 @@ class Mentor(object):
 	def get_interests(self):
 		return self.interests
 
+	def calc_pair_score(self, mentee, gender_weight, course_code_weight, interest_weight):
+		gender_score = 1.5 * gender_weight
+		course_code_score = 1.5 * course_code_weight
+		interest_score = 1 * interest_weight
+
+		pairing_score = 0
+		if self.gender == mentee.get_gender():
+			pairing_score += gender_score
+
+		if self.course_code == mentee.get_course_code():
+			pairing_score += course_code_score
+
+		for interest in self.interests:
+			if interest in mentee.get_interests():
+				pairing_score += interest_score
+
+		return(mentee, pairing_score)
+
+	def set_preferences(self, menteeList, gender_weight, course_code_weight, interest_weight):
+		for mentee in menteeList:
+			pairing = self.calc_pair_score(mentee, gender_weight, course_code_weight, interest_weight)
+			self.preferences.append(pairing)
+
+		print(self.preferences)
+		#sort preferences
+		#unpack preference tuples
+
+
+
+
+
 
 
 class Mentee(object):
 
-	def __init__(self, gender, course_code, student_id, interests):
+	def __init__(self, gender, course_code, student_id, interests, preferences=[]):
 		self.gender = gender
 		self.course_code = course_code
 		self.student_id = student_id
 		self.interests = interests
+		self.preferences = []
 
 	def __str__(self):
 		return f"Mentee: {self.student_id}"
@@ -49,7 +82,12 @@ class Mentee(object):
 
 
 def main():
-	pass
+	mentor1 = Mentor("other", "nothing", 1, ["boo", "boo", "boo"])
+	mentor2 = Mentor("female", "POPD", 2, ["eating", "skiing", "shoes"])
+	mentor3 = Mentor("male", "CASE", 3, ["football", "programming", "pints"])
+	mentor4 = Mentor("male", "CASE", 4, ["tennis", "basketball", "pints"])
+
+	mentorList = [mentor1, mentor2, mentor3, mentor4]
 
 
 if __name__ == '__main__':
